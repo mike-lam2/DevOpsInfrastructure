c95521fc54d3d4cd6e656335fa1cc14a03457ccf
mike-lam2 <mike-lam2@example.com>
2016-03-04 15:56:31 -0500

    .

--- ConfigurationManagement/git_test.php
+++ ConfigurationManagement/git_test.php
@@ -15,5 +15,5 @@ file_put_contents('logs/github.txt', print_r($payload, TRUE), FILE_APPEND);
 if ($payload->ref === 'refs/heads/master')
 {
   // path to your site deployment script
-  exec('./build.sh');
+  exec('/var/www/html/DevOpsApplication/build.sh');
 }
