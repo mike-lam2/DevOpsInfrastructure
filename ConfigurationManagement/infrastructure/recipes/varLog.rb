104c32c322e671e87587cc2e0a6691c9d29a6e18
mike-lam2 <author@example.com>
2016-03-12 09:50:49 -0500

    fix varlog

--- ConfigurationManagement/infrastructure/recipes/varLog.rb
+++ ConfigurationManagement/infrastructure/recipes/varLog.rb
@@ -1,4 +1,9 @@
-link '/var/log' do
-  to '/var/www/html/log'
+link '/var/www/html/log' do
+  to '/var/log'
+  link_type :symbolic
+end
+
+link '/var/www/html/buildNumber.txt' do
+  to '/home/ec2-user/buildNumber.txt'
   link_type :symbolic
 end
