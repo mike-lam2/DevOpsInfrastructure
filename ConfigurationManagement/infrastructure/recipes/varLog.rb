ac315f0750551a8a3472811d99eae4eccfe0944c
mike-lam2 <author@example.com>
2016-03-12 11:54:39 -0500

    .

--- ConfigurationManagement/infrastructure/recipes/varLog.rb
+++ ConfigurationManagement/infrastructure/recipes/varLog.rb
@@ -2,8 +2,3 @@ link '/var/www/html/log' do
   to '/var/log'
   link_type :symbolic
 end
-
-link '/var/www/html/buildVersion.txt' do
-  to '/home/ec2-user/buildVersion.txt'
-  link_type :symbolic
-end
