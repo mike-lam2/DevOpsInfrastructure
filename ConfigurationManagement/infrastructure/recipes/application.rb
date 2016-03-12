416a8e3c85a6b640ca2dc7d77d0876b8b6b9de33
mike-lam2 <author@example.com>
2016-03-12 17:49:46 -0500

    adding chef to webserver

---
+++ ConfigurationManagement/infrastructure/recipes/application.rb
@@ -0,0 +1,108 @@
+gitsite = node.default['default']['Gitsite']
+application= node.default['default']['Appplication']
+branch_name = node.default['default']['BranchToClone']
+repo=gitsite+appplication+".git"
+
+git '/var/www/html/'+application do
+   repository repo
+   revision branch_name
+   action :sync
+   user 'apache'
+   group 'apache'
+end
+
+
+
+=begin
+
+            "commands":{
+              "0015-chmod-html":{
+                "command":"sudo chmod a+w html",
+                "cwd":"/var/www",
+                "ignoreErrors":"true"
+              },
+              "0020-git-Application":{
+                "command":{
+                  "Fn::Join":[
+                    "",
+                    [
+                      "git clone https://github.com/mike-lam2/",
+                      {
+                        "Ref":"Application"
+                      },
+                      " --branch ",
+                      {
+                        "Ref":"BranchToClone"
+                      }
+                    ]
+                  ]
+                },
+                "cwd":"/var/www/html",
+                "ignoreErrors":"true"
+              },
+              "0030-chmod-build":{
+                "command":{
+                  "Fn::Join":[
+                    "",
+                    [
+                      "sudo chmod a+x ",
+                      {
+                        "Ref":"Application"
+                      },
+                      "/build.sh"
+                    ]
+                  ]
+                },
+                "cwd":"/var/www/html",
+                "ignoreErrors":"true"
+              },
+              "0040-build":{
+                "command":{
+                  "Fn::Join":[
+                    "",
+                    [
+                      "sudo chmod a+x ",
+                      {
+                        "Ref":"Application"
+                      },
+                      "/build.sh"
+                    ]
+                  ]
+                },
+                "cwd":"/var/www/html",
+                "ignoreErrors":"true"
+              },
+              
+              "0050-chown-Application":{
+                "command":{
+                  "Fn::Join":[
+                    "",
+                    [
+                      "sudo chown -R apache ",
+                      {
+                        "Ref":"Application"
+                      }
+                    ]
+                  ]
+                },
+                "cwd":"/var/www/html",
+                "ignoreErrors":"true"
+              },
+              "0060-chgrp-Application":{
+                "command":{
+                  "Fn::Join":[
+                    "",
+                    [
+                      "sudo chgrp -R apache ",
+                      {
+                        "Ref":"Application"
+                      }
+                    ]
+                  ]
+                },
+                "cwd":"/var/www/html",
+                "ignoreErrors":"true"
+     
+   ]
+   
+=end
