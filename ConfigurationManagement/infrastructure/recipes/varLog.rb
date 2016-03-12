7275baddb9b068f31a6bdc06405cd3a29b4da4b4
mike-lam2 <author@example.com>
2016-03-12 12:20:36 -0500

    .

--- ConfigurationManagement/infrastructure/recipes/varLog.rb
+++ ConfigurationManagement/infrastructure/recipes/varLog.rb
@@ -1,3 +1,9 @@
+remote_file '/var/log/buildAttributes.txt' do
+  source '/var/chef/chef-repo/cookbooks/infrastructure/attributes/default.rb'
+  mode '0444'
+  action :create
+end
+
 link '/var/www/html/log' do
   to '/var/log'
   link_type :symbolic
