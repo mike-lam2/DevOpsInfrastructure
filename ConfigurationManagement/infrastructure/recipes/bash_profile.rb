59777ad4d998f087b0aa23fa42376967f0e9a92b
mike-lam2 <author@example.com>
2016-03-11 21:36:27 -0500

    cf+chef 1st try

---
+++ ConfigurationManagement/infrastructure/recipes/bash_profile.rb
@@ -0,0 +1,22 @@
+file '/home/ec2-user/.bash_profile' do
+  action :create
+  group 'ec2-user'
+  owner 'ec2-user'
+  content '
+# .bash_profile
+
+# Get the aliases and functions
+if [ -f ~/.bashrc ]; then
+        . ~/.bashrc
+fi
+
+# User specific environment and startup programs
+
+PATH=$PATH:$HOME/.local/bin:$HOME/bin:.
+PS1='.../${PWD##*/} \> '
+
+
+export PATH
+export PS1
+'
+end
