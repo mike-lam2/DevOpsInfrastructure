e7cba730f4b0c2c8daf7d5bdb2866ccb4525d03b
mike-lam2 <author@example.com>
2016-03-12 01:15:48 -0500

    .

--- ConfigurationManagement/infrastructure/recipes/xterm.rb
+++ ConfigurationManagement/infrastructure/recipes/xterm.rb
@@ -1,8 +1,10 @@
+StackName = node.default['default']['StackName']
+label='echo -ne "\033]0;${USER}@'+StackName+':${PWD/#$HOME/~}\007"'
 file '/etc/sysconfig/bash-prompt-xterm' do
   action :create
   mode '0555'
   group 'root'
   owner 'root'
-  content 'echo -ne "\033]0;${USER}@XXX:${PWD/#$HOME/~}\007"'
+  content label
 end
 
