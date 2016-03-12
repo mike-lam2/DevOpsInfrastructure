bf855b5b7b90dd5794606c93d5254c7306749526
mike-lam2 <author@example.com>
2016-03-12 13:35:55 -0500

    partial cfn-hup in chef

---
+++ ConfigurationManagement/infrastructure/recipes/cfn.rb
@@ -0,0 +1,42 @@
+template '/etc/cfn/cfn-hup.conf' do
+  source 'cfn-hup.conf.erb'
+  owner 'root'
+  group 'root'
+  mode '0400'
+end
+
+template '/etc/cfn/hooks.d/cfn-auto-reloader.conf' do
+  source 'cfn-auto-reloader.conf'
+  owner 'root'
+  group 'root'
+  mode '0400'
+end
+
+link '/var/www/html/cfn' do
+  to '/etc/cfn'
+  link_type :symbolic
+end
+
+#=beging is same as /* in java
+=begin
+              "0015-chmod-html":{
+                "command":"sudo chmod -R a+r *",
+                "cwd":"/etc/cfn",
+                "ignoreErrors":"true"
+              }
+            },
+            "services":{
+              "sysvinit":{
+                "cfn-hup":{
+                  "enabled":"true",
+                  "ensureRunning":"true",
+                  "files":[
+                    "/etc/cfn/cfn-hup.conf",
+                    "/etc/cfn/hooks.d/cfn-auto-reloader.conf"
+                  ]
+                }
+              }
+            }
+          }
+        }
+=end
