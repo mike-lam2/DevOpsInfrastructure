ec35e810d1dacaf4d2265130420b008d312aa001
mike-lam2 <author@example.com>
2016-03-12 21:04:53 -0500

    .

--- ConfigurationManagement/infrastructure/recipes/httpd.rb
+++ ConfigurationManagement/infrastructure/recipes/httpd.rb
@@ -1,6 +1,13 @@
 package 'httpd'
 package 'php'
 
+directory '/var/www/html' do
+  owner 'apache'
+  group 'apache'
+  mode '0664'
+  action :create
+end
+
 service 'httpd' do
   action [ :enable, :start ]
 end
